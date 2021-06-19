c0c3608f7cb1d4d57c472060e099e45f4699ab8c
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-19 11:17:36 +0530

    artifacts

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,9 @@
-import os
+import os as windows
 name = "Jithendar"
 full_name = name + " Dharmapuri"
 age = 29
 good_name = "Jithu"
 print(full_name)
 print('my name is ' + full_name + ' people call me as ' + good_name)
-print('Hello, ' + os.getlogin() + '! How are you?')
-print('my age is ' = age)
+print('Hello, ' + windows.getlogin() + '! How are you?')
+print(f'my age is ' + str(age))
