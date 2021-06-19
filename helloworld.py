98ab7d728c5bb57094a88d4a4cad0ca1f8c78949
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-19 12:35:04 +0530

    python

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,24 @@
-#import os
-name = "Jithendar"
-full_name = name + " Dharmapuri"
-age = 29
-good_name = "Jithu"
-print(full_name)
-print('my name is ' + full_name + ' people call me as ' + good_name)
-#print('Hello, ' + os.getlogin() + '! How are you?')
-print(f'my age is ' + str(age))
+# import os
+# name = "Jithendar"
+# full_name = name + " Dharmapuri"
+# age = 29
+# good_name = "Jithu"
+# print(full_name)
+# print('my name is ' + full_name + ' people call me as ' + good_name)
+# print('Hello, ' + os.getlogin() + '! How are you?')
+# print(f'my age is ' + str(age))
 
+# fname = "Jithendar"
+# sname = "Dharmapuri"
+# if fname == "abc":
+#     print(f'my first name is: ' + fname)
+# elif sname != "Dharmapuri":
+#     print(f'my second name is: ' + sname)
+# else:
+#     print('my full name is ' + full_name)
+
+DevOps_tools = ["terraform", "ansible", "jenkins", "docker", "kubernetes"]
+for tools in DevOps_tools:
+    print(tools)
+    if tools == "docker":
+        break
