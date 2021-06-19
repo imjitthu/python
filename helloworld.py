ac8fa95b557d47816a6d812ee8b5e46229e518eb
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-19 11:21:55 +0530

    python

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,9 @@
-import os as windows
+import os
 name = "Jithendar"
 full_name = name + " Dharmapuri"
 age = 29
 good_name = "Jithu"
 print(full_name)
 print('my name is ' + full_name + ' people call me as ' + good_name)
-print('Hello, ' + windows.getlogin() + '! How are you?')
+print('Hello, ' + os.getlogin() + '! How are you?')
 print(f'my age is ' + str(age))
