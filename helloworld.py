52fc4429b0f753f05262d403d0cb25addebbd0f9
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-19 11:14:24 +0530

    artifacts

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,7 @@
+import os
 name = "Jithendar"
 full_name = name + " Dharmapuri"
 good_name = "Jithu"
 print(full_name)
-print (f"hello" (good_name))
+print('my name is ' + full_name + ' people call me as ' + good_name)
+print('Hello, ' + os.getlogin() + '! How are you?')
