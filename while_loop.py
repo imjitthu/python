1f0a0858043e1432995c27de29ad6966710b4cf2
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-20 08:58:31 +0530

    python

--- while_loop.py
+++ while_loop.py
@@ -1,11 +1,14 @@
 count = 1
 while True:
     print(f'the count is {count}')
-    if count > 9:
+    if count >= 10:
         break
-    count += 1
+    count+= 1
 
 count = 1
 while count < 10:
     print(f'the count us {count}')
     count +=1
+
+#In Python 3.6, the f-string was introduced(PEP 498). 
+#In short, it is a way to format your string that is more readable and fast.
